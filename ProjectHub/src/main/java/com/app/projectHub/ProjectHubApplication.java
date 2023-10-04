02fa0e81d1d9bed910cb0f36f2cb32ccc6d6b78a
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-10-04 20:13:10 +0530

    Project-HUb-backend-init

---
+++ ProjectHub/src/main/java/com/app/projectHub/ProjectHubApplication.java
@@ -0,0 +1,13 @@
+package com.app.projectHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHubApplication.class, args);
+	}
+
+}
---
+++ ProjectHub/src/test/java/com/app/projectHub/ProjectHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.projectHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
